e27c2cce7fc3220da6f9636d8e3e4fe7191d800b
rxdey <jyh1994@qq.com>
2023-06-09 00:55:04 +0800

    ✨ feat(query): nsfw

--- src/modules/favorite/update.js
+++ src/modules/favorite/update.js
@@ -4,11 +4,11 @@ import select from '../../db/query.js';
 const update = async ({ body, auth }, { sendErrorResponse, sendSuccessResponse }) => {
     const { id: uid } = auth;
     const typeList = ['img', 'url', 'text'];
-    const { id, tid, type, content, origin, title, desc_txt, preview_img, is_show, download = true } = body;
+    const { id, tid, type, content, origin, title, desc_txt, preview_img, is_show, nsfw, download = true } = body;
     if (!id) return sendErrorResponse({ msg: '更新失败，未查询到ID' });
     if (type && !typeList.includes(type)) return sendErrorResponse({ msg: '更新失败，错误的类型' });
     const sets = {
-        tid, type, content, origin, title, desc_txt, preview_img, is_show, update_date: dateformat()
+        tid, type, content, origin, title, desc_txt, preview_img, is_show, nsfw, update_date: dateformat()
     };
     try {
         // 更新数据，返回更新条数
