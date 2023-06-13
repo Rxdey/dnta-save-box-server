2dd253d767911c99acb3496ca2618dc5928a14f2
jiaoyuhan <jyh1994@qq.com>
2023-06-13 17:47:52 +0800

    重置排序

---
+++ src/modules/favorite/updateSort.js
@@ -0,0 +1,25 @@
+import { dateformat, cleanObj } from '../../utils/index.js';
+import select from '../../db/query.js';
+
+/**
+ * 重设全部排序字段值
+ * @param {*} param0 
+ * @param {*} param1 
+ * @returns 
+ */
+const updateSort = async ({ body, auth }, { sendErrorResponse, sendSuccessResponse }) => {
+    const { id: uid } = auth;
+
+    try {
+        const initSql = 'SET @row_number := 0;';
+        const updateSql = `UPDATE favorite SET sort = (@row_number := @row_number + 1) * 200 WHERE uid = ${uid} ORDER BY sort DESC, create_date DESC;`;
+        await select.querySql(initSql);
+        const record = await select.querySql(updateSql);
+        return sendSuccessResponse({ data: record, msg: '更新成功' });
+    } catch (error) {
+        console.log(error);
+        return sendErrorResponse({ msg: '更新失败' });
+    }
+};
+
+export default updateSort;
